2de3d75f3e42423880d105467c89e26ab391e6f0
MuhdSHiBiLi <jameela.poonthala786@gmail.com>
2024-01-04 20:05:48 +0530

    first

---
+++ function.js
@@ -0,0 +1,18 @@
+function sub(a,b){
+    var s = a-b;
+    console.log(s);
+};
+
+function mul(a,b){
+    var s = a*b;
+    console.log(s);
+};
+
+function div(a,b){
+    var s = a/b;
+    console.log(s);
+};
+
+sub(5,15);
+mul(30,-10);
+div(5,10);
---
+++ script.js
@@ -0,0 +1,30 @@
+// let person = {
+//     name:"shibili",
+//     age:12,
+//     place:"Puliyakkode"
+// };
+
+// // console.log(person.name)
+
+// var ar =  [{name:"shibili",age:12},{name:"ashil",age:14}];
+// console.log(ar[1].age);
+
+// var a = 12;
+// var b = 13;
+// if (a==b){
+//     console.log("a is equal to b");
+// }
+// else if(a>b){
+//     con
+
+// }
+
+let a = 5;
+// console.log(a++);
+// let b = a++; // b =a (b kk adhyam a value kodkkum then a nte value koottum);
+let b = ++a; // a= a+1 (adyam a pluse then valuew b kk kodkkum), b =a;
+console.log("a equals", a);
+console.log("b equals",b);
+
+
+
